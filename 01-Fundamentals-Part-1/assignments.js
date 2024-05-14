f56bce39da8776df5d306987d365a54e5308b044
christina-baldwin <christina.baldwin13@yahoo.com>
2024-05-14 15:15:03 +0200

    Switch statement assignment

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,4 +1,4 @@
-/* let country = "Sweden";
+let country = "Sweden";
 let continent = "Europe";
 let language = "English";
 let population = 10.5;
@@ -67,4 +67,25 @@ if (language === "English" && population < 50 && !isIsland) {
   console.log(`You should live in ${country} :)`);
 } else {
   console.log(`${country} does not meet your criteria :(`);
-} */
+}
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
+    break;
+  case "spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "english":
+    console.log("3rd place");
+    break;
+  case "hindi":
+    console.log("Number 4");
+    break;
+  case "arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
