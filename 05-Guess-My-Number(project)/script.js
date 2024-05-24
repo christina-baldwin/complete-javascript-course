caa40444c23d5d2ad2d84cc7b50bcffe8d1ede8c
christina-baldwin <christina.baldwin13@yahoo.com>
2024-05-24 14:46:56 +0200

    Clicke events

--- 05-Guess-My-Number(project)/script.js
+++ 05-Guess-My-Number(project)/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct number!';
 
@@ -8,4 +8,15 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value); */
+
+// this makes sure that the number that is inputed in the number box by the user gets saved when they click "check"
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // if there is no guess inputted before clicking check
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
