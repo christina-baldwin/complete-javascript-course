9616e871f74401b8126f3c7d68a4a409259cf294
christina-baldwin <christina.baldwin13@yahoo.com>
2024-05-25 10:41:14 +0200

    Implementing the game logic and reset game functions

--- 05-Guess-My-Number(project)/script.js
+++ 05-Guess-My-Number(project)/script.js
@@ -10,13 +10,65 @@ document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); */
 
+// defining the secret number using a random number generator function
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+//
+
+// creating a score variable that will decrease by 1 with every wrong guess, better to create a varibale to hold our data
+let score = 20;
+
 // this makes sure that the number that is inputed in the number box by the user gets saved when they click "check"
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
-  // if there is no guess inputted before clicking check
+  // game logic function
   if (!guess) {
+    // no input: if there is no guess inputted before clicking check
     document.querySelector('.message').textContent = 'No number!';
+
+    // when player wins: guess is the same as secret number, page style changes
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // guess is too high: guess greater than secret number, attempts decrease by 1, losing message when attmepts = 0, not letting it go below 0
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //guess is too low: guess is lower than secret number, attempts decrease by 1, losinbg message when attempts = 0, not letting it go below 0
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+// reset the game when clicking "again"
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
