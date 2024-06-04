c31efbea98deb20465c0f5fcd0a8bcda4d42248f
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-04 13:18:13 +0200

    Functions and primitives vs objects

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function calcAge(birthYear) {
+/* function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
@@ -33,4 +33,167 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
-//console.log(age); //cant access becuase the scope chain can't reach inside the funtion
+//console.log(age); //cant access becuase the scope chain can't reach inside the funtion */
+
+// HOISTING
+//variables
+/*console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;*/
+
+//functions
+/*console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;*/
+
+// example
+/*console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);*/
+
+//THIS KEYWORD
+/*console.log(this); //the base "this" keyword is for window
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge; // method borrowing
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f(); */
+
+/*var firstName = 'Matilda';
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // solution 1
+    // const self = this; // do this to override the issue with calling functions as they call "this" as underfined, if we create a variable for "this" outside of the funtion it works the way we want
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log((this.year >= 1981 && this.year <= 1996));
+    // };
+
+    // solution 2
+    // can fix the initial problem using an arrow function as it will take the "this" of the parent scope
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+// this is not a code block it is just an object literal
+// arrow functions do not have "this" so just call from general which is window
+jonas.greet();
+jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12); // can do this: they exist but they are not named
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
+// arguments keyword does not exist in an arrow function
+// not important in modern JS */
+
+// PRIMITIVES VS OBJECTS
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davies';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davies'; // can change properties within a const object because the address doesnt change, just the value in the memory
+console.log('Before marriage', jessica);
+console.log('After marriage', marriedJessica);
+
+//marriedJessica = {} the entire object cannot be changed
+
+// copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davies';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
