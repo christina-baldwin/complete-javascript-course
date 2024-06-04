cf9e3e2e35b0ecbbfe3399a22a255d876c7126bd
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-04 15:52:45 +0200

    destructuring objects

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -1,7 +1,7 @@
+// DESTRUCTURING ARRAYS
 // 1.1
 const books = ['salemsLot', 'dracula', 'dune'];
 const [firstBook, secondBook] = books;
-console.log(firstBook, secondBook);
 
 // 1.2
 const [, , thirdBook] = books;
@@ -20,3 +20,30 @@ const ratingStars = [63405, 1808];
 const [fiveStarRatings = 0, oneStarRatings = 0, threeStarRatings = 0] =
   ratingStars;
 console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+
+// DESTRUCTURING OBJECTS
+// 2.1
+const { title, author, ISBN } = books[0];
+// 2.2
+const { keywords: tags } = books[0];
+// 2.3
+const { language, programmingLanguage = 'unknown' } = books[6];
+// 2.4
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+({ title: bookTitle, author: bookAuthor } = books[0]);
+// 2.5
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = books[0];
+// 2.6
+printBookInfo({
+  title: 'Algorithms',
+  author: 'Robert Sedgewick',
+  year: '2011',
+});
+function printBookInfo({ title, author, year = 'year unknown' }) {
+  console.log(`${title} by ${author}, ${year}`);
+}
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -30,8 +30,61 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
+    );
+  },
 };
 
+// practical application (see function above, used destructuring in the original function using curly brackets and used defaults)
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via Del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via Del Sole, 21',
+  starterIndex: 1,
+});
+
+// DESTRUCTURING OBJECTS
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+// changing names
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+
+// nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*/////////////////////////
+// DESTRUCTURING ARRAYS
 const arr = [2, 3, 4];
 const a = [0];
 const b = [1];
@@ -65,4 +118,4 @@ console.log(i, j, k);
 
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r); // without default values, r is equal to undefined
+console.log(p, q, r); // without default values, r is equal to undefined */
