65966d134888af151c7137bb7135631edd076fa8
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-04 13:59:36 +0200

    destructuring arrays assignment

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -1,3 +1,22 @@
 // 1.1
-const books = ['salemsLot', 'dracula'];
-[firstBook, secondBook] = books;
+const books = ['salemsLot', 'dracula', 'dune'];
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+
+// 1.2
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+// 1.3
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+[[, rating], [, ratingsCount]] = ratings;
+console.log(ratings, ratingsCount);
+
+// 1.4
+const ratingStars = [63405, 1808];
+const [fiveStarRatings = 0, oneStarRatings = 0, threeStarRatings = 0] =
+  ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
