26418717904bc0d440ba51a94edf91fefd0415a1
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-10 17:05:04 +0200

    Preparing forlder for next section (functions), practice with default parameters

---
+++ 10-Functions/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// default values
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2);
+createBooking('LH123', undefined, 1000);
