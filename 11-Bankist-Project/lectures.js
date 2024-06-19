b395c095d94c32e01a07630a0da8fcd25fbbfa64
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-19 15:56:13 +0200

    Starting arrays challenge #4

--- 11-Bankist-Project/lectures.js
+++ 11-Bankist-Project/lectures.js
@@ -237,3 +237,18 @@ const avg4 = calcAverageHumanAge1([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg3, avg4);
 
 // CHALLENGE #4
+// 1.
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+const calcRecommendedFood = function (dogs) {
+  dogs.forEach(function (dog) {
+    dog.recommendedFood = Math.trunc((dog.weight ** 0.75 * 28) / 100);
+  });
+};
+calcRecommendedFood(dogs);
+console.log(dogs);
+// 2.
