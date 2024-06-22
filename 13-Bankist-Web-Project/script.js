7e06cd01bef0ec521297aa72c06ceb14fbf5917f
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-22 18:18:03 +0200

    Practicing event propagation and bubbling

--- 13-Bankist-Web-Project/script.js
+++ 13-Bankist-Web-Project/script.js
@@ -103,6 +103,9 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+/*/////////////////////////////////
+// NOT RELATED TO WEBSITE
+
 ////////////////////////////////
 // mouseenter event listeners
 const h1 = document.querySelector('h1');
@@ -114,3 +117,27 @@ const alertH1 = function (e) {
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+////////////////////////////////
+// event propagation/bubbling
+// rgb(255,255,255)
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1));
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+
+  // stop propagation
+  e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target);
+}); */
