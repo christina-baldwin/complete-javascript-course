c29b8b3509ca72266a06d27df4324304632f9281
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-28 18:50:19 +0200

    Fetch and promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -6,7 +6,68 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // building a UI component with country information
 
-// our first AJAX call
+///////////////////////////////////////
+// fetch api - new way
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} million people</p>
+      <p class="country__row"><span>🗣️</span>${
+        data.languages[Object.keys(data.languages)[0]]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        data.currencies[Object.keys(data.currencies)[0]].name
+      }</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const request = fetch('https://restcountries.com/v3.1/name/uk');
+console.log(request);
+
+const getCountryData = function (country) {
+  // fetches something
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    // we then get a response which we return
+    .then(function (response) {
+      // then need to read the data from the response, this returns a new promise
+      return response.json();
+    })
+    // since the previous was a promise, we can then chain the "then" method onto it
+    .then(function (data) {
+      // country 1
+      renderCountry(data[0]);
+      console.log(data);
+      // country 2
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      return renderCountry(data[0], 'neighbour');
+    });
+};
+getCountryData('uk');
+
+/*///////////////////////////////////////
+// our first AJAX call - old way
 
 const renderCountry = function (data, className = '') {
   const html = `
@@ -63,17 +124,18 @@ const getCountryAndNeighbourData = function (country) {
   });
 };
 
-getCountryAndNeighbourData('uk');
+getCountryAndNeighbourData('uk'); */
 
-setTimeout(() => {
-  console.log('1 second passed');
-  setTimeout(() => {
-    console.log('2 seconds passed');
-    setTimeout(() => {
-      console.log('3 second passed');
-      setTimeout(() => {
-        console.log('4 second passed');
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+// callback hell
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
