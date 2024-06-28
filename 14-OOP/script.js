a53c73776c63f6a64e93f368efe8757e5bbba2c2
christina-baldwin <christina.baldwin13@yahoo.com>
2024-06-28 12:30:59 +0200

    Finishing off the OOP section

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -192,6 +192,128 @@ console.log(mike instanceof Student);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
 
+// inheritance between ES6 classes
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // always needs to happen first, creates the "this" keyword
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`Hi my name is ${this.fullName} and I study ${this.course}`);
+  }
+  // overwrites the method with the same name from the parent class
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
+
+// inheritance with Objoect.create
+const PersonProto2 = {
+  calcAge() {
+    console.log(2037 - birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+  introduce() {
+    console.log(`Hi my name is ${this.firstName} and I study ${this.course}`);
+  },
+};
+
+const steven2 = Object.create(PersonProto2);
+
+const StudentProto2 = Object.create(PersonProto2);
+StudentProto2.init = function (firstName, birthYear, course) {
+  PersonProto2.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+const jay = Object.create(StudentProto2);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+
+// class example
+// private class fields and methods (very new)
+// 1. public fields
+// 2. private fields
+// 3. public methods
+// 4. public methods
+// there is also a static public method where they only work on the class itself
+class Account {
+  // 1. public fields (instances, need a ;)
+  locale = navigator.language;
+
+  // 2. private fields
+  #transactions = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // can do this if we want something that doesnt need to be determined when making a new object
+    // this._transactions = [];
+    this.locale = navigator.language;
+    this.#pin = pin;
+
+    console.log(`Thanks for opening an account ${this.owner}`);
+  }
+  // 3. public methods
+  // public interface: can read but cannot set them
+  getTransactions() {
+    return this.#transactions;
+  }
+  // create methods to interact with properties instead of doing it just outside
+  deposit(val) {
+    this.#transactions.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  // this should only be able to access approveLoan, not be accessibel outise of this, this a data privacy concern
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+  // 4. private methods
+  #approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+// acc1.#approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.getTransactions());
+
+// console.log(acc1.#transactions);
+
 ////////////////////////////////
 // CODING CHALLENGES
 
@@ -240,6 +362,7 @@ class CarCl {
 
   brake() {
     console.log((this.speed -= 5));
+    return this;
   }
 
   get speedUS() {
@@ -258,3 +381,49 @@ car3.accelerate();
 car3.accelerate();
 car3.speedUS = 50;
 console.log(car3);
+
+// challenge #3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  console.log(`Speed: ${(this.speed += 20)}\nCharge: ${this.charge--}`);
+};
+
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+tesla.chargeBattery(90);
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+
+// challenge #4
+class EVCl extends CarCl {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    console.log(`Speed: ${(this.speed += 20)}\nCharge: ${this.#charge}%`);
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+rivian.accelerate().accelerate().brake().chargeBattery(80).accelerate();
