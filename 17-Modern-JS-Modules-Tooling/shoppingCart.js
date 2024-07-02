798d8eba5678baf2a9323c1a7ee51a01c92fe808
christina-baldwin <christina.baldwin13@yahoo.com>
2024-07-02 14:48:53 +0300

    Setting up Parcel

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,12 +1,17 @@
 // Exporting module
 console.log('Exporting module');
 
+// Blocking code
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
+
 const shippingCost = 10;
 export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`Added to cart: ${product}, ${quantity}`);
+  console.log(`${quantity} ${product} added to cart`);
 };
 
 const totalPrice = 237;
@@ -16,5 +21,5 @@ export { totalPrice, totalQuantity as tq };
 
 export default function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`Added to cart: ${product}, ${quantity}`);
+  console.log(`${quantity} ${product} added to cart`);
 }
